a01851d55a030b5df61159a7ed4788fe27d6a05c
ziko7110 <ziko7110@gmail.com>
2021-06-14 21:23:04 +0900

    2-2-3

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,44 @@
 <template>
-
+    <main class="main">
+        <button 
+            class="startBtn"
+            @click="appStart"
+            v-if="isStarted==false">
+            Start
+        </button>
+        <section
+            class="touchZone"
+            v-else>
+            Started
+        </section>
+    </main>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            isStarted: false
+        }    
+    },
+    methods: {
+        appStart(){
+            this.isStarted = true
+        }
+    }
 }
 </script>
 
 <style>
+.main{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 
+.startBtn{
+    padding: 10px 20px;
+}
 </style>
