837ff1e00a4c868096607643636e7b39fa6e2df7
ziko7110 <ziko7110@gmail.com>
2021-06-15 21:57:51 +0900

    2-2

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
         <section
             class="touchZone"
             v-else>
-            Started
+            &nbsp;
         </section>
     </main>
 </template>
@@ -40,5 +40,13 @@ export default {
 
 .startBtn{
     padding: 10px 20px;
+}
+
+.touchZone{
+    background: linear-gradient( 45deg, rgb(255,255,255),rgb(100,255,255) );
+    width: 100%;
+    min-width: 100%;
+    height: 100vh;
+    min-height: 100vh;
 }
 </style>
